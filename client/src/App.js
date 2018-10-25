f4b1755a2c07fb9c352a8019b225074cc3f26c8d
Bharath Kumar <kumarbharath718@gmail.com>
2018-10-26 00:15:56 +0530

    create pulse animation effect using svg

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
@@ -19,10 +18,20 @@ class App extends Component {
   };
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
+      <div className="Pulse">
+        <svg width="108" 
+          height="128" 
+          viewBox="0 0 54 64" 
+          xmlns="http://www.w3.org/2000/svg">
+          <g id="Page-1" 
+            fill="none" 
+            fillRule="evenodd">
+            <path className="beat-loader" 
+              d="M0.5,38.5 L16,38.5 L19,25.5 L24.5,57.5 L31.5,7.5 L37.5,46.5 L43,38.5 L53.5,38.5"
+              id="Path-2" 
+              strokeWidth="2" />
+          </g>
+        </svg>
         <p className="App-intro">{this.state.response}</p>
       </div>
     );
